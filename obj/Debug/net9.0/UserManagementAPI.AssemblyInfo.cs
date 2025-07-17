96176a03c02c87b4a6f9a3294c954804cd033c8c
Umesh <aggarwalumesh@1@gmail.com>
2025-07-17 18:22:10 +0530

    build-run

--- obj/Debug/net9.0/UserManagementAPI.AssemblyInfo.cs
+++ obj/Debug/net9.0/UserManagementAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f121c31d59a921f61b65480fe51dff7030fabb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+137e676f54d2d9b315f0146d9b93a37fd3d7cb80")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
